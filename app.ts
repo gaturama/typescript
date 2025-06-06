f503babad8eb8cadc86a24f2d1ccbb0d19b9a5e1
Gabriel <bielsobv@gmail.com>
2025-06-06 20:44:51 -0300

    Using set and get

--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, "Gabriel ", 21)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "Gabriel", 21)
+console.log(peopleAccount)
 peopleAccount.deposit()
-
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 25)
-console.log(companyAccount.deposit())
+// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 25)
+// console.log(companyAccount.deposit())
