fe4a039c0f64cde80a75a6ca93fce1b6195be8c5
Gabriel <bielsobv@gmail.com>
2025-06-06 21:01:12 -0300

    Practice using readonly

--- app.ts
+++ app.ts
@@ -2,8 +2,9 @@ import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
 
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, "Gabriel", 21)
-console.log(peopleAccount)
-peopleAccount.deposit()
-// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 25)
-// console.log(companyAccount.deposit())
+// const peopleAccount: PeopleAccount = new PeopleAccount(1, "Gabriel", 21)
+// console.log(peopleAccount)
+// peopleAccount.deposit()
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 25)
+// companyAccount.deposit()
+console.log(companyAccount)
